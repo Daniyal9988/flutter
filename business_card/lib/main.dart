7e1295bc039484528851193406ffc854c57725af
Daniyal9988 <sdani9988@gmail.com>
2024-04-02 05:33:06 +0300

    added business card app

--- business_card/lib/main.dart
+++ business_card/lib/main.dart
@@ -1,10 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-//stateless widget statless shortcut to get the full widget
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -15,42 +16,72 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            //crossAxisAlignment: CrossAxisAlignment.stretch, // stretchs the child to make max width
-            //crossAxisAlignment: CrossAxisAlignment.end, //  this does not do anything if all child have same width
-            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,//space evenly between childs
-            //mainAxisAlignment: MainAxisAlignment.center, // to align the childs
-            // verticalDirection: VerticalDirection.up, // inverts the vertical direction of the column
-            //mainAxisSize: MainAxisSize.min, // makes the widget take  only the size needed
-            children: [
-              Container(
-                height: 100.0,
-                width: 100.0,
-                //margin: const EdgeInsets.all(100.0), // to position parent
-                // padding: const EdgeInsets.all(20.0), // to position the child
+            mainAxisAlignment: MainAxisAlignment.center, // to center the column
+            children: const [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/profil.jpeg'),
+              ),
+              Text(
+                "Syed Daniyal Saeed",
+                style: TextStyle(
+                  fontFamily: 'Pacifico', // custom font
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "FULL STACK DEVELOPER",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'source sans',
+                  fontSize: 20.0,
+                  color: Color.fromARGB(255, 134, 197, 192),
+                  letterSpacing: -1.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 190.0,
+                child: Divider(
+                  color: Color.fromARGB(255, 101, 218, 190),
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "+60195352749",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal,
+                        fontFamily: "source sans",
+                      ),
+                    ),
+                  )),
+              Card(
                 color: Colors.white,
-                child: const Text("hello"),
-              ),
-              const SizedBox(
-                // to have a space between childs of column
-                height: 10.0,
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.red,
-                child: const Text("hello 2"),
-              ),
-
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.blue,
-                child: const Text("hello 3"),
-              ),
-              // to align column vertically we can use invivisble container
-              Container(
-                width: double
-                    .infinity, //  this makes the container to take the width as wide as screen allows
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "sdani9988@gmail.com",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal,
+                        fontFamily: "source sans",
+                      ),
+                    )),
               )
             ],
           ),
@@ -59,3 +90,28 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+/* this is one way of adding instead of listtile
+ Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "sdani9988@gmail.com",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal,
+                        fontFamily: "source sans",
+                      ),
+                    ),
+                  ],
+                ),
+
+*/
